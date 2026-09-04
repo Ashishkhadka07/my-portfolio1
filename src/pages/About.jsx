1dfc6011d2785c84d56b9a597828b0585c002c38
Ashishkhadka07 <aasheeshkhadka@gmail.com>
2026-09-04 11:24:20 +0545

    about bio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
+import AboutBio from "../components/about/AboutBio";
 import AboutHero from "../components/about/AboutHero";
 
 export default function About() {
   return (
     <main className="relative w-full overflow-x-hidden bg-[#111319]">
       <AboutHero />
+      <AboutBio />
     </main>
   );
 }
